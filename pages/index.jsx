f2c87c36756f5fa45b0eddb7f3f06c789e327a02
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-08 20:40:26 +0600

    If the user is logged in, redirect them to the /elections page

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,18 @@ import Head from 'next/head';
 import AccountsPage from '../layouts/accountsPage';
 import AccountsForm from '../components/accountsForm';
 
+import { auth } from '../api/firebase';
 
 class Home extends Component {
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        Router.push('/elections');
+      } else {
+      }
+    });
+  }
+
   render() {
     return (
       <AccountsPage>
