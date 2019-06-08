834684798e6003d6a0d881e4c0970fdeca0aa934
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-08 22:24:54 +0600

    Move firebase config to a different file
    Altho I could commit the api keys, I kinda don't want to.
    
    This is silly but meh

--- api/firebase.js
+++ api/firebase.js
@@ -2,9 +2,11 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+import config from './config';
+
 // Initialize Firebase
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 }
 
 const auth = firebase.auth();
