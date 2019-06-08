e7e24704365144e80ed0b779e19f9cf718fd5060
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-08 22:47:19 +0600

    Add postcss-nested plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 const tailwindcss = require('tailwindcss');
 
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')]
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    require('postcss-nested'),
+    require('autoprefixer')
+  ]
 };
