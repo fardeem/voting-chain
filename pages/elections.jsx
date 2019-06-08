b8a25d18ea561e3a4bad4a78afa86806482e3657
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-08 20:40:49 +0600

    Create election.jsx

---
+++ pages/elections.jsx
@@ -0,0 +1,13 @@
+/**
+ * This page shows all the elections.
+ * If a election id is passed in, it shows the nomination/voting page accordingly
+ */
+
+import React from 'react';
+import Router from 'next/router';
+
+
+export default () => (
+  <div>
+  </div>
+);
