e073a4aff2eb6ce1bd42713dbde571a83cd3864a
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-08 20:38:24 +0600

    Add a helper function to manage auth errors

--- api/firebase.js
+++ api/firebase.js
@@ -10,5 +10,26 @@ if (!firebase.apps.length) {
 const auth = firebase.auth();
 const db = firebase.firestore();
 
+/**
+ * Add a function to return error messages given an error message
+ * for authentication
+ */
+auth.handleError = function handleError(errorCode) {
+  switch (errorCode) {
+    case 'auth/wrong-password':
+      return 'Wrong password.';
+    case 'auth/invalid-email':
+      return 'Email not valid.';
+    case 'auth/user-not-found':
+      return 'No user exists with this email address.';
+    case 'auth/email-already-in-use':
+      return 'Email is already being used by another account.';
+    case 'auth/weak-password':
+      return 'Weak password.';
+    default:
+      return '';
+  }
+};
+
 export default firebase;
 export { auth, db };
