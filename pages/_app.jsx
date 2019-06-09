bc91fec1aecc17db6b12b5b33b2578e0d224bb26
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-09 19:13:39 +0600

    Add a favicon. Remove unneccesary import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import App, { Container } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/main.css';
 
@@ -15,6 +16,9 @@ export default class MyApp extends App {
 
     return (
       <Container>
+        <Head>
+          <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </Container>
     );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Router from 'next/router';
-import Head from 'next/head';
 
 import AccountsPage from '../layouts/accountsPage';
 import AccountsForm from '../components/accountsForm';
