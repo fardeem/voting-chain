7fc37542d100dfba81b7a74d0f7bc5d8a9e7bdba
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-10 19:32:59 +0600

    Pull in all UI components for the election page

--- pages/elections.jsx
+++ pages/elections.jsx
@@ -4,20 +4,30 @@
  */
 
 import React from 'react';
-import Router from 'next/router';
+import { withRouter } from 'next/router';
 
-import { auth } from '../api/firebase';
+import ElectionsNav from '../components/ElectionsNav';
+import ElectionsTitle from '../components/ElectionsTitle';
+import ElectionsList from '../components/ElectionsList';
 
-export default () => (
-  <div>
-    <button
-      className="bg-blue-400 text-white p-4"
-      onClick={() => {
-        auth.signOut();
-        Router.push('/');
-      }}
-    >
-      Sign Out
-    </button>
-  </div>
-);
+const ElectionsPage = ({ router }) => {
+  return (
+    <main>
+      <header className="bg-pattern">
+        <div className="w-full max-w-4xl mx-auto px-8 pt-6 pb-4 mb-6">
+          <ElectionsNav />
+
+          <ElectionsTitle electionId={router.query.id} />
+        </div>
+      </header>
+
+      <div className="content w-full rounded-t-lg -mt-2 bg-white">
+        <div className="w-full max-w-4xl mx-auto px-8 pt-10">
+          <ElectionsList />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default withRouter(ElectionsPage);
