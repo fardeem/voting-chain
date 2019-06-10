1a080b22228a0c0df609cdce32589286863ac620
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-10 19:32:29 +0600

    Create UI for election page nav

---
+++ components/ElectionsNav.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+
+import { auth } from '../api/firebase';
+
+const ElectionsNav = () => (
+  <div className="flex justify-between">
+    <nav>
+      <Link href="/elections">
+        <a className="text-gray-300 hover:text-white text-sm uppercase font-bold tracking-wider">
+          All Elections
+        </a>
+      </Link>
+    </nav>
+
+    <div className="accounts flex items-center">
+      <p className=" text-gray-100 mr-4 tracking-wide">Fardeem</p>
+      <button
+        onClick={() => {
+          auth.signOut();
+          Router.push('/');
+        }}
+        className="bg-pink-500 cursor-pointer hover:bg-pink-600 text-sm font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline"
+      >
+        Logout
+      </button>
+    </div>
+  </div>
+);
+
+export default ElectionsNav;
