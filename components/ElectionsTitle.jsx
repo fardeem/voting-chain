969b7613e23fd00fc6c4c9b220dea388a206c0cb
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-11 20:36:58 +0600

    Hook up election title component to use the provider

--- components/ElectionsTitle.jsx
+++ components/ElectionsTitle.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 
+import ElectionsProvider from '../api/ElectionsProvider';
+
 const ElectionsTitle = ({ electionId }) => {
-  const headerTitle = electionId ? `Election #${electionId}` : 'All Elections';
+  const { elections } = useContext(ElectionsProvider);
+
+  const headerTitle =
+    electionId && elections.length !== 0
+      ? elections.find(e => e.id === electionId).name
+      : 'All Elections';
 
   return (
     <>
@@ -10,12 +17,9 @@ const ElectionsTitle = ({ electionId }) => {
         <title>{headerTitle}</title>
       </Head>
 
-      <div className="text-white text-center mt-12">
-        <h1 className="text-6xl font-light">{headerTitle}</h1>
-        <h2 className="font-serif italic text-lg text-pink-500">
-          6 elections are being voted on. 3 are being nominated.
-        </h2>
-      </div>
+      <h1 className="text-6xl font-light text-white text-center mt-12">
+        {headerTitle}
+      </h1>
     </>
   );
 };
