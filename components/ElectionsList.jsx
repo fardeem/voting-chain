90aa7bf02abba02e02ddbd9c5d9c2d6dc88639dd
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-11 16:38:07 +0600

    Add section for past elections

--- components/ElectionsList.jsx
+++ components/ElectionsList.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 
+const EmptySection = () => (
+  <div className="empty font-extrabold text-3xl select-none text-gray-300">
+    Nothing To
+    <br /> See Here
+  </div>
+);
+
 const ElectionsList = () => (
   <div className="flex mb-4">
-    <div className="w-1/2 pr-4">
+    <div className="w-1/3 mr-4">
       <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 mb-6 uppercase tracking-wide">
         Voting
       </h1>
@@ -11,32 +18,35 @@ const ElectionsList = () => (
       <ul>
         <li className="mb-4">
           <Link href="/elections?id=123">
-            <a className="text-xl hover:text-pink-600">
+            <a className="text-md hover:text-pink-600">
               Toronto Raptors Roster
             </a>
           </Link>
 
-          <p className="italic font-serif text-gray-600">
+          <p className="italic text-sm font-serif text-gray-600">
             Voting closes in 1 day, 53 minutes
           </p>
         </li>
         <li className="mb-4">
-          <h2 className="text-xl">Golden State Warriors Roster</h2>
-          <p className="italic font-serif text-gray-600">
+          <h2 className="text-md">Golden State Warriors Roster</h2>
+          <p className="italic text-sm font-serif text-gray-600">
             Voting closes in 1 day, 53 minutes
           </p>
         </li>
       </ul>
     </div>
-    <div className="w-1/2 text-right">
-      <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 uppercase tracking-wide">
+    <div className="w-1/3 mr-4">
+      <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 mb-6 uppercase tracking-wide">
         Nominating
       </h1>
+      <EmptySection />
+    </div>
 
-      <div className="empty font-extrabold text-5xl select-none text-gray-300 mt-8">
-        Nothing To
-        <br /> See Here
-      </div>
+    <div className="w-1/3">
+      <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 mb-6 uppercase tracking-wide">
+        Past Election
+      </h1>
+      <EmptySection />
     </div>
   </div>
 );
