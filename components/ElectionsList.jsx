81f80bc269f19ef466747804686ce859820375f8
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-11 20:30:28 +0600

    Refactor election list to use the provider

--- components/ElectionsList.jsx
+++ components/ElectionsList.jsx
@@ -1,54 +1,59 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
+import moment from 'moment';
 
-const EmptySection = () => (
-  <div className="empty font-extrabold text-3xl select-none text-gray-300">
-    Nothing To
-    <br /> See Here
-  </div>
-);
+import ElectionsContext from '../api/ElectionsProvider.jsx';
 
-const ElectionsList = () => (
-  <div className="flex mb-4">
-    <div className="w-1/3 mr-4">
-      <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 mb-6 uppercase tracking-wide">
-        Voting
-      </h1>
+const ElectionsList = () => {
+  const { elections } = useContext(ElectionsContext);
+  const electionStates = ['voting', 'nominating', 'done'];
 
-      <ul>
-        <li className="mb-4">
-          <Link href="/elections?id=123">
-            <a className="text-md hover:text-pink-600">
-              Toronto Raptors Roster
-            </a>
+  return (
+    <div className="flex mb-4">
+      {electionStates.map(state => (
+        <div className="w-1/3 mr-4" key={state}>
+          <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 mb-6 uppercase tracking-wide">
+            {state === 'done' ? 'Past elections' : state}
+          </h1>
+
+          <List
+            data={elections.filter(e => e.status === state)}
+            status={state}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const List = ({ data, status }) => {
+  return data.length !== 0 ? (
+    <ul>
+      {data.map(({ id, name, start, end }) => (
+        <li key={id}>
+          <Link href={`/elections?id=${id}`}>
+            <a className="text-md hover:text-pink-600">{name}</a>
           </Link>
 
           <p className="italic text-sm font-serif text-gray-600">
-            Voting closes in 1 day, 53 minutes
+            {(() => {
+              if (status === 'voting')
+                return `Voting ends ${moment(end.toISOString()).fromNow()}`;
+              else if (status === 'nominating')
+                return `Voting begins ${moment(start.toISOString()).fromNow()}`;
+              else
+                return `Election ended ${moment(end.toISOString()).fromNow()}`;
+            })()}
           </p>
         </li>
-        <li className="mb-4">
-          <h2 className="text-md">Golden State Warriors Roster</h2>
-          <p className="italic text-sm font-serif text-gray-600">
-            Voting closes in 1 day, 53 minutes
-          </p>
-        </li>
-      </ul>
+      ))}
+    </ul>
+  ) : (
+    <div className="empty font-extrabold text-3xl select-none text-gray-300">
+      Nothing To
+      <br /> See Here
     </div>
-    <div className="w-1/3 mr-4">
-      <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 mb-6 uppercase tracking-wide">
-        Nominating
-      </h1>
-      <EmptySection />
-    </div>
-
-    <div className="w-1/3">
-      <h1 className="text-sm font-bold border-pink-500 border-b-2 pb-1 mb-6 uppercase tracking-wide">
-        Past Election
-      </h1>
-      <EmptySection />
-    </div>
-  </div>
-);
+  );
+};
 
 export default ElectionsList;
