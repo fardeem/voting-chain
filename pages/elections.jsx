e6172b973c6cd337e98a5096aa3409f0a6d4b6eb
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-12 18:20:54 +0600

    Create pages for voting, nominating and results

--- pages/elections.jsx
+++ pages/elections.jsx
@@ -3,27 +3,57 @@
  * If a election id is passed in, it shows the nomination/voting page accordingly
  */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { withRouter } from 'next/router';
 
+import DataProvider from '../api/DataProvider';
 import ElectionsNav from '../components/ElectionsNav';
 import ElectionsTitle from '../components/ElectionsTitle';
 import ElectionsList from '../components/ElectionsList';
+import NominationsPage from '../components/NominationsPage';
+import VotingPage from '../components/VotingPage';
 
 const ElectionsPage = ({ router }) => {
+  const { elections } = useContext(DataProvider);
+
+  const electionId = router.query.id;
+  let currentElection;
+  if (electionId) currentElection = elections.find(e => e.id === electionId);
+
   return (
-    <main>
+    <main className="min-h-screen">
       <header className="bg-pattern">
         <div className="w-full max-w-4xl mx-auto px-8 pt-6 pb-4 mb-6">
           <ElectionsNav />
 
-          <ElectionsTitle electionId={router.query.id} />
+          <ElectionsTitle electionId={electionId} />
         </div>
       </header>
 
       <div className="content w-full rounded-t-lg -mt-2 bg-white">
         <div className="w-full max-w-4xl mx-auto px-8 pt-10">
-          <ElectionsList />
+          {/* 
+            if query.id
+              if voting
+                votingpage
+              else
+                nominationspage
+            else
+              Elections list
+          
+          */}
+
+          {(() => {
+            if (electionId && currentElection) {
+              if (currentElection.status === 'voting')
+                return <VotingPage context={currentElection} />;
+              else if (currentElection.status === 'nominating')
+                return <NominationsPage context={currentElection} />;
+              else return <h1>Results</h1>;
+            } else {
+              return <ElectionsList />;
+            }
+          })()}
         </div>
       </div>
     </main>
