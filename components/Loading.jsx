509ae3cffd478b1af311b3cfac7b53cc650be784
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-12 22:41:53 +0600

    Create loading component

---
+++ components/Loading.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+
+class Loading extends Component {
+  componentDidMount = () => {
+    NProgress.start();
+  };
+
+  componentWillUnmount = () => {
+    console.log('unmounting');
+    NProgress.done();
+  };
+
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Loading</title>
+        </Head>
+        <div className="w-full h-screen bg-pattern flex justify-center items-center">
+          <span className="text-white text-4xl uppercase tracking-widest font-extrabold">
+            Loading
+          </span>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Loading;
