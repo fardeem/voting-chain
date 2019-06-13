2ae9dc47911fb66c288c4d9dc19898cc107bb3eb
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-13 23:22:21 +0600

    Make UI for admin form component

--- components/AdminCreateForm.jsx
+++ components/AdminCreateForm.jsx
@@ -3,6 +3,54 @@ import React from 'react';
 const AdminCreateForm = ({ show }) => {
   return (
     <div className="absolute flex justify-center w-full">
+      <div
+        className={
+          'w-full max-w-xs mt-10 shadow-2xl bg-white rounded p-8 transition-opacity ' +
+          (show ? 'z-10 opacity-100' : 'z-hide opacity-0')
+        }
+      >
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+            <p className="text-red-500 text-xs italic">
+              Please choose a password.
+            </p>
+          </div>
+
+          <button
+            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Create
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
