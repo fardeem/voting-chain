572211876e5df0fb32bbaf488b445e1a61702734
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-13 23:18:00 +0600

    Create admin page

---
+++ pages/admin.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+
+// TODO: Authenticate properly
+const AdminPage = () => {
+
+  return (
+    <main
+    >
+      <Head>
+        <title>Admin</title>
+      </Head>
+    </main>
+  );
+};
+
+export default AdminPage;
