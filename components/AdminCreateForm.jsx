bd09b16d2beec58ffa03d6d79dbd4787ddab8591
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-15 00:48:24 +0600

    Make UI for adding new positions

--- components/AdminCreateForm.jsx
+++ components/AdminCreateForm.jsx
@@ -42,15 +42,67 @@ const AdminCreateForm = ({ show }) => {
             <p className="text-red-500 text-xs italic">
               Please choose a password.
             </p>
+              <PositionsList
+                list={positionsList}
+                updateList={setPositionsList}
           </div>
 
           <button
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            className="bg-purple-500 mx-auto block mt-6 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
           >
             Create
           </button>
         </form>
+      </div>
+    </div>
+  );
+};
+
+const PositionsList = ({ list, updateList }) => {
+  const [newPosition, setNewPosition] = useState('');
+
+  function addNewPosition() {
+    const key = newPosition.toLowerCase().replace(' ', '-');
+    updateList(Object.assign({}, list, { [key]: newPosition }));
+    setNewPosition('');
+  }
+
+  return (
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2">
+        Positions
+      </label>
+
+      <ul className="mb-4">
+        {Object.keys(list).map(key => (
+          <li
+            key={key}
+            className="inline-block px-2 py-1 bg-gray-300 rounded-lg mr-3 mb-3 text-sm"
+          >
+            {list[key]}
+          </li>
+        ))}
+      </ul>
+
+      <div className="flex">
+        <input
+          className="shadow flex-1 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Add Positions"
+          value={newPosition}
+          onChange={e => setNewPosition(e.target.value)}
+          onKeyPress={e => {
+            if (e.key === 'Enter') addNewPosition();
+          }}
+        />
+        <button
+          className="bg-blue-500 ml-2 shadow hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          onClick={addNewPosition}
+        >
+          Add
+        </button>
       </div>
     </div>
   );
