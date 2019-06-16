f2f7938b1ee5455b5f40dd27ac3677c27b74deef
Fardeem Munir <fardeem.munir@erubai.com>
2019-06-16 18:55:08 +0600

    Disable default value for the select box

--- components/NominationsPage.jsx
+++ components/NominationsPage.jsx
@@ -43,7 +43,7 @@ const NominationsPage = ({ context }) => {
 };
 
 const NominateUser = ({ options, position, electionId }) => {
-  const [selectedUser, setSelectedUser] = useState('');
+  const [selectedUser, setSelectedUser] = useState('default');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -69,11 +69,13 @@ const NominateUser = ({ options, position, electionId }) => {
     <form className="w-full max-w-sm mb-6" onSubmit={handleSubmit}>
       <div className="flex">
         <select
-          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 mr-3 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 mr-3 px-2 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
           value={selectedUser}
           onChange={e => setSelectedUser(e.target.value)}
         >
-          <option value="" />
+          <option value="default" disabled>
+            Select a user
+          </option>
           {options.map(user => (
             <option key={user.id} value={user.id}>
               {user.name}
