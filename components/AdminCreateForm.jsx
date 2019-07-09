5ca9e968a19cd9186dac3b75e05bb2d80330efcc
Fardeem Munir <fardeem.munir@erubai.com>
2019-07-09 12:38:29 +0600

    Remove form and attach handlesubmit function to button click

--- components/AdminCreateForm.jsx
+++ components/AdminCreateForm.jsx
@@ -7,8 +7,7 @@ const AdminCreateForm = ({ show, setShow }) => {
   const [end, setEnd] = useState('');
   const [positionsList, setPositionsList] = useState({});
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  function handleSubmit() {
 
     db.collection('elections')
       .add({
@@ -39,69 +38,65 @@ const AdminCreateForm = ({ show, setShow }) => {
         }
         onClick={e => e.stopPropagation()}
       >
-        <form onSubmit={handleSubmit}>
-          <div className="flex">
-            <div className="w-1/2 pr-6">
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">
-                  Name
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="text"
-                  required
-                  value={name}
-                  onChange={e => setName(e.target.value)}
-                />
-              </div>
-
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">
-                  Start Date
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  type="date"
-                  value={start}
-                  required
-                  onChange={e => {
-                    setStart(e.target.value);
-                  }}
-                  max={end}
-                />
-              </div>
-              <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2">
-                  End Date
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  type="date"
-                  value={end}
-                  required
-                  onChange={e => {
-                    setEnd(e.target.value);
-                  }}
-                  min={start}
-                />
-              </div>
+        <div className="flex">
+          <div className="w-1/2 pr-6">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                required
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
             </div>
 
-            <div className="w-1/2 pl-6 border-gray-300 border-l-2 border-dashed">
-              <PositionsList
-                list={positionsList}
-                updateList={setPositionsList}
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Start Date
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="date"
+                value={start}
+                required
+                onChange={e => {
+                  setStart(e.target.value);
+                }}
+                max={end}
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                End Date
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                type="date"
+                value={end}
+                required
+                onChange={e => {
+                  setEnd(e.target.value);
+                }}
+                min={start}
               />
             </div>
           </div>
 
-          <button
-            className="bg-purple-500 mx-auto block mt-6 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Create
-          </button>
-        </form>
+          <div className="w-1/2 pl-6 border-gray-300 border-l-2 border-dashed">
+            <PositionsList list={positionsList} updateList={setPositionsList} />
+          </div>
+        </div>
+
+        <button
+          className="bg-purple-500 mx-auto block mt-6 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          onClick={handleSubmit}
+        >
+          Create
+        </button>
       </div>
     </div>
   );
