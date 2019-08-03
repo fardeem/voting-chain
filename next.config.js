931e01ba3c02fc06439f7d69c3a61d6c98298a85
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-03 14:31:02 +0600

    Add a web worker to mine blocks

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 const withCSS = require('@zeit/next-css');
+const withWorkers = require('@zeit/next-workers');
 
-module.exports = withCSS();
+module.exports = withWorkers(
+  withCSS({
+    webpack(config, { isServer }) {
+      if (!isServer) {
+        // Without this, web workers doesn't work properly
+        config.output.globalObject = 'self';
+      }
+
+      return config;
+    }
+  })
+);
