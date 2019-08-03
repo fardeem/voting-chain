900174dc3357b6e52fcceef4eb7b6dcf9db2e18f
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-03 21:19:33 +0600

    Add route to broadcast new blocks

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 8500;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 require('./io').initialize(http);
 const io = require('./io').io();
@@ -18,8 +19,19 @@ const io = require('./io').io();
  * Routes
  */
 
-app.get('/', function(_, res) {
-  res.sendFile(__dirname + '/index.html');
+// app.get('/', function(_, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+app.post('/new-vote', function(req, res) {
+  io.emit('MINE', req.body);
+  res.send('Received');
+});
+
+app.post('/new-block', function(req, res) {
+  console.log('BLOCK', req.body);
+  io.emit('BLOCK', req.body);
+  res.send('Received');
 });
 
 /**
