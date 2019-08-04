c6db7a535e89d18a5341a04a79422697aa289728
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-04 14:34:49 +0600

    Show current blockchain in the blockchain page

--- pages/blockchain.jsx
+++ pages/blockchain.jsx
@@ -3,7 +3,7 @@ import React, { Component, useState, useEffect, useContext } from 'react';
 import BlockchainContext from '../api/blockchain';
 
 const Socket = () => {
-  const { castVote, miningQueue } = useContext(BlockchainContext);
+  const { blockchain, castVote, miningQueue } = useContext(BlockchainContext);
 
   return (
     <div>
@@ -23,13 +23,28 @@ const Socket = () => {
           Add vote
         </button>
 
-        <div>
+        {/* <div>
           <h1 className="text-lg">In queue</h1>
           <ul>
             {miningQueue.map((vote, index) => (
               <li key={index}>
                 From {vote.from} to {vote.to} for {vote.electionId} in position{' '}
                 {vote.electionId}
+              </li>
+            ))}
+          </ul>
+        </div> */}
+
+        <hr />
+
+        <div className="border-gray-900 border-t-2">
+          <h1 className="text-2xl">Blockchain</h1>
+
+          <ul>
+            {blockchain.map((block, index) => (
+              <li key={index}>
+                Hash: {block.hash}, previousHash: {block.previousHash}, vote:{' '}
+                {JSON.stringify(block.vote)}
               </li>
             ))}
           </ul>
