c133b90c482daf7650770bd981946a97f64ec6a9
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-06 20:35:40 +0600

    Seperate nominating user component from page component file

--- components/NominationsPage.jsx
+++ components/NominationsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 
 import DataProvider from '../api/DataProvider';
-import firebase, { db } from '../api/firebase';
+import NominateUser from './NominateUser';
 
 const NominationsPage = ({ context }) => {
   const { users } = useContext(DataProvider);
@@ -39,58 +39,6 @@ const NominationsPage = ({ context }) => {
         </div>
       ))}
     </div>
-  );
-};
-
-const NominateUser = ({ options, position, electionId }) => {
-  const [selectedUser, setSelectedUser] = useState('default');
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    // if (selectedUser)
-    db.collection('elections')
-      .doc(electionId)
-      .update({
-        [`nominations.${position}`]: firebase.firestore.FieldValue.arrayUnion(
-          selectedUser
-        )
-      })
-      .then(() => {
-        setSelectedUser('');
-      });
-
-    console.log(
-      `Nominate user ${selectedUser} to position with key ${position} for election with id ${electionId}`
-    );
-  }
-
-  return (
-    <form className="w-full  mb-6" onSubmit={handleSubmit}>
-      <div className="flex">
-        <select
-          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 mr-3 px-2 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-          value={selectedUser}
-          onChange={e => setSelectedUser(e.target.value)}
-        >
-          <option value="default" disabled>
-            Select a user
-          </option>
-          {options.map(user => (
-            <option key={user.id} value={user.id}>
-              {user.name}
-            </option>
-          ))}
-        </select>
-
-        <button
-          className="flex-shrink-0 bg-purple-500 hover:bg-purple-600 border-purple-500 hover:border-purple-600 text-sm border-4 text-white py-1 px-2 rounded"
-          type="submit"
-        >
-          Nominate
-        </button>
-      </div>
-    </form>
   );
 };
 
