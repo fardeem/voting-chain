08d6c0e429c45229438a46504dbb3f3d635286e2
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-08 11:40:11 +0600

    Move blockchain utility files to separate file

--- api/blockchain.tsx
+++ api/blockchain.tsx
@@ -13,7 +13,14 @@ import io from 'socket.io-client';
 import Miner from './miner.worker';
 import ec from './curve';
 import DataContext from './DataProvider';
-import { auth } from './firebase';
+import {
+  blockchainReducer,
+  genesisBlock,
+  hashVote,
+  getLongestChain,
+  broadcastToNetwork,
+  castVote
+} from './utils';
 
 const socket = io('localhost:8500');
 
@@ -21,7 +28,7 @@ const socket = io('localhost:8500');
 // Interfaces
 //====================
 
-interface VoteInfo {
+export interface VoteInfo {
   to: string;
   electionId: string;
   position: string;
@@ -33,7 +40,7 @@ export interface Vote extends VoteInfo {
   signature: string;
 }
 
-interface Block {
+export interface Block {
   hash: string;
   previousHash: string;
   nonce: number;
@@ -154,110 +161,3 @@ export const BlockchainProvider = ({ children }) => {
     </BlockchainContext.Provider>
   );
 };
-
-//====================
-// Helper Functions
-//====================
-
-function hashVote(vote: Vote): string {
-  return shajs('sha256')
-    .update(
-      vote.from + vote.to + vote.electionId + vote.position + vote.timestamp
-    )
-    .digest('hex');
-}
-
-function castVote({ to, electionId, position }: VoteInfo, privateKey: string) {
-  const key = ec.keyFromPrivate(privateKey, 'hex');
-
-  const vote: Vote = {
-    to,
-    electionId,
-    position,
-    timestamp: Date.now(),
-    from: auth.currentUser.uid,
-    signature: ''
-  };
-
-  const voteHash = hashVote(vote);
-  vote.signature = key.sign(voteHash, 'base64').toDER('hex');
-
-  return broadcastToNetwork(JSON.stringify(vote), 'VOTE');
-}
-
-type BroadcastAction = 'BLOCK' | 'VOTE';
-function broadcastToNetwork(body: string, action: BroadcastAction) {
-  let endpoint = '';
-  const method = 'POST';
-  const headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  };
-
-  if (action === 'BLOCK') endpoint = 'new-block';
-  else if (action === 'VOTE') endpoint = 'new-vote';
-
-  return fetch(`http://localhost:8500/${endpoint}`, {
-    method,
-    headers,
-    body
-  });
-}
-
-const genesisBlock: Block = {
-  hash: '0008a5469e5cb0d2c2844a18efaf8fc723e100b08c34485b784e04fce79166f9',
-  previousHash: '0',
-  nonce: 194,
-  vote: null
-};
-
-function getLongestChain(blockchain: Block[]) {
-  const found = [];
-
-  function buildLink(parent: Block, history = []) {
-    var next = blockchain.filter(item => item.previousHash === parent.hash);
-
-    if (next.length === 0) {
-      return found.push([parent, ...history]);
-    } else {
-      return next.forEach(item => {
-        return buildLink(item, [parent, ...history]);
-      });
-    }
-  }
-
-  buildLink(blockchain.find(block => block.previousHash === '0'));
-
-  return found.sort((a, b) => b.length - a.length)[0];
-}
-
-type BlockchainActions = { type: 'UPDATE'; value: Block[] };
-
-function blockchainReducer(blockchain: Block[], action: BlockchainActions) {
-  if (action.type !== 'UPDATE') throw new Error('HOBE NAH TOH!');
-
-  const updates = [];
-  action.value.forEach(newBlock => {
-    const calculatedHash = shajs('sha256')
-      .update(
-        JSON.stringify(newBlock.vote) +
-          newBlock.previousHash +
-          String(newBlock.nonce)
-      )
-      .digest('hex');
-
-    if (
-      blockchain.find(
-        block =>
-          block.hash === newBlock.hash &&
-          block.previousHash === newBlock.previousHash
-      ) &&
-      calculatedHash === newBlock.hash
-    )
-      return;
-
-    updates.push(newBlock);
-  });
-
-  return [...blockchain, ...updates];
-}
---
+++ api/utils.tsx
@@ -0,0 +1,114 @@
+import shajs from 'sha.js';
+import ec from './curve';
+
+import { VoteInfo, Vote, Block } from './blockchain';
+import { auth } from './firebase';
+
+export function hashVote(vote: Vote): string {
+  return shajs('sha256')
+    .update(
+      vote.from + vote.to + vote.electionId + vote.position + vote.timestamp
+    )
+    .digest('hex');
+}
+
+export function castVote(
+  { to, electionId, position }: VoteInfo,
+  privateKey: string
+) {
+  const key = ec.keyFromPrivate(privateKey, 'hex');
+
+  const vote: Vote = {
+    to,
+    electionId,
+    position,
+    timestamp: Date.now(),
+    from: auth.currentUser.uid,
+    signature: ''
+  };
+
+  const voteHash = hashVote(vote);
+  vote.signature = key.sign(voteHash, 'base64').toDER('hex');
+
+  return broadcastToNetwork(JSON.stringify(vote), 'VOTE');
+}
+
+type BroadcastAction = 'BLOCK' | 'VOTE';
+export function broadcastToNetwork(body: string, action: BroadcastAction) {
+  let endpoint = '';
+  const method = 'POST';
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  };
+
+  if (action === 'BLOCK') endpoint = 'new-block';
+  else if (action === 'VOTE') endpoint = 'new-vote';
+
+  return fetch(`http://localhost:8500/${endpoint}`, {
+    method,
+    headers,
+    body
+  });
+}
+
+export const genesisBlock: Block = {
+  hash: '0008a5469e5cb0d2c2844a18efaf8fc723e100b08c34485b784e04fce79166f9',
+  previousHash: '0',
+  nonce: 194,
+  vote: null
+};
+
+export function getLongestChain(blockchain: Block[]) {
+  const found = [];
+
+  function buildLink(parent: Block, history = []) {
+    var next = blockchain.filter(item => item.previousHash === parent.hash);
+
+    if (next.length === 0) {
+      return found.push([parent, ...history]);
+    } else {
+      return next.forEach(item => {
+        return buildLink(item, [parent, ...history]);
+      });
+    }
+  }
+
+  buildLink(blockchain.find(block => block.previousHash === '0'));
+
+  return found.sort((a, b) => b.length - a.length)[0];
+}
+
+type BlockchainActions = { type: 'UPDATE'; value: Block[] };
+
+export function blockchainReducer(
+  blockchain: Block[],
+  action: BlockchainActions
+) {
+  if (action.type !== 'UPDATE') throw new Error('HOBE NAH TOH!');
+
+  const updates = [];
+  action.value.forEach(newBlock => {
+    const calculatedHash = shajs('sha256')
+      .update(
+        JSON.stringify(newBlock.vote) +
+          newBlock.previousHash +
+          String(newBlock.nonce)
+      )
+      .digest('hex');
+
+    if (
+      blockchain.find(
+        block =>
+          block.hash === newBlock.hash &&
+          block.previousHash === newBlock.previousHash
+      ) &&
+      calculatedHash === newBlock.hash
+    )
+      return;
+
+    updates.push(newBlock);
+  });
+
+  return [...blockchain, ...updates];
+}
