fa72f7a471975601049764d31025aef9524e810f
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-11 23:50:27 +0600

    Use custom useElectionResults hook on results page

--- components/ResultsPage.tsx
+++ components/ResultsPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { useRouter } from 'next/router';
+import DataContext from '../api/DataProvider';
+import { useElectionResult } from '../api/results';
+
+function randomVote() {
+  return Math.floor(Math.random() * 10) + 1;
+}
 
 const ResultsPage = () => {
-  return <h1>Results</h1>;
+  const router = useRouter();
+  const { id } = router.query;
+  const result = useElectionResult(id);
+
+  return <div>Results</div>;
 };
 
 export default ResultsPage;
