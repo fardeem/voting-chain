c6399adddabb641ed7082ddad248c90596ebdd03
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-12 22:02:08 +0600

    Make wrapper for hold resolve page

--- pages/admin/[id].jsx
+++ pages/admin/[id].jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import DataContext from '../../api/DataProvider';
+import { useElectionResult } from '../../api/useResults';
+import AdminLayout from '../../layouts/AdminLayout';
 
 const Page = () => {
   const router = useRouter();
   const { id } = router.query;
+  const { elections } = useContext(DataContext);
+  const { name } = elections.find(election => election.id === id);
 
-  return <h1>{id}</h1>;
+  const [results, positionsOnHold] = useElectionResult(id);
+
+  return (
+    <AdminLayout
+      title={name}
+      menuItems={
+        <Link href="/admin">
+          <a className="text-white bg-purple-600 cursor-pointer hover:bg-purple-500 text-sm font-bold py-2 px-4 rounded focus:outline-none">
+            All Elections
+          </a>
+        </Link>
+      }
+    >
+      {positionsOnHold.length > 0 ? (
+        <h1>Resolve Hold</h1>
+      ) : (
+        <h1>Show Results</h1>
+      )}
+    </AdminLayout>
+  );
 };
+
 export default Page;
