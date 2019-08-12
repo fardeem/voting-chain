9feda6217b2ff4404670934f641c9f9b372d4cbc
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-12 07:28:05 +0600

    Add some bottom padding to accounts page layout

--- layouts/accountsPage.jsx
+++ layouts/accountsPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const AccountsPage = ({ children }) => (
-  <main className="bg-pattern pt-20 min-h-screen">
-    <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <main className="bg-pattern pt-20 pb-4 min-h-screen">
+    <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8">
       {children}
     </div>
   </main>
