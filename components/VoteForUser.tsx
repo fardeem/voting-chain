b0dde81588ef8eb49934dc6ba91002386b763e3b
Fardeem Munir <fardeem.munir@erubai.com>
2019-08-13 13:09:13 +0600

    Fix label and retake screenshots

--- components/VoteForUser.tsx
+++ components/VoteForUser.tsx
@@ -63,7 +63,7 @@ const VoteForUser = ({ options, position, electionId }: Props) => {
           className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
         >
           <option value="default" disabled>
-            Select your option
+            Select candidate
           </option>
 
           {options.map(user => {
